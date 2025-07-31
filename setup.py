aca6420bc70d0d26293840c2349b3e9e72ed23db
zyrafaq <krzesimir@hobler.pl>
2025-07-31 13:33:56 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='banguard-api',
+    version='0.1.0',
+    author='Zyrafaq',
+    author_email='contact@zyrafaq.com',
+    description='Python API wrapper for BanGuard API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/zyrafaq/BanGuard-API/',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
