8f83acfcc72c95b13dade1a32d97c4f0e4d4483c
zyrafaq <krzesimir@hobler.pl>
2025-08-11 16:20:01 +0200

    bump version (forgot dependencies)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='banguard-api',
-    version='0.1.4',
+    version='0.1.5',
     author='Zyrafaq',
     author_email='contact@zyrafaq.com',
     description='Python API wrapper for BanGuard API',
